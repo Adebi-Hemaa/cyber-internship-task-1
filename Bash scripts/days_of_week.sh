2c77f82ce17b65cf73cf34ffe4a4b9d6c7f3f38c
Adebi-Hemaa <adebiyeboah091@gmail.com>
2025-10-07 18:53:53 -0500

    assignments

---
+++ Bash scripts/days_of_week.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo "Enter a number between 1 and 7: "
+read number
+
+case $number in
+    1)
+        echo "Monday"
+        ;;
+    2)
+        echo "Tuesday"
+        ;;
+    3)
+        echo "Wednesday"
+        ;;
+    4)
+        echo "Thursday"
+        ;;
+    5)
+        echo "Friday"
+        ;;
+    6)
+        echo "Saturday"
+        ;;
+    7)
+        echo "Sunday"
+        ;;
+    *)
+        echo "Invalid input"
+        ;;
+esac
